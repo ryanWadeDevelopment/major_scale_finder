e01e150ef5ba6474ef8ebd32bf8a3ab09644bee0
Ryan Wade <ryanwadedevelopment@gmail.com>
2022-08-11 09:35:20 -0500

    Added scale display skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ScalePickerComponent } from './scale_picker/scale_picker.component';
+import { ScaleDisplayComponent } from './scale_display/scale_display.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, ScalePickerComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ScalePickerComponent,
+    ScaleDisplayComponent,
+  ],
   imports: [BrowserModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/scale_display/scale_display.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scale-display',
+  templateUrl: './scale_display.component.html',
+  styleUrls: ['./scale_display.component.css'],
+})
+export class ScaleDisplayComponent {}
