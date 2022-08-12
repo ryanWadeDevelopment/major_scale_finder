060b50cbd9219d1c429108581a3e656d54048e57
Ryan Wade <ryanwadedevelopment@gmail.com>
2022-08-12 14:21:32 -0500

    Added Major Scale model

--- src/app/scale_display/scale_display.component.ts
+++ src/app/scale_display/scale_display.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MajorScale } from '../shared/MajorScale.Model';
 
 @Component({
   selector: 'app-scale-display',
   templateUrl: './scale_display.component.html',
   styleUrls: ['./scale_display.component.css'],
 })
-export class ScaleDisplayComponent {}
+export class ScaleDisplayComponent {
+  CScale = new MajorScale('C', 'D', 'E', 'F', 'G', 'A', 'B');
+}
---
+++ src/app/shared/MajorScale.Model.ts
@@ -0,0 +1,11 @@
+export class MajorScale {
+  constructor(
+    public note1: string,
+    public note2: string,
+    public note3: string,
+    public note4: string,
+    public note5: string,
+    public note6: string,
+    public note7: string
+  ) {}
+}
